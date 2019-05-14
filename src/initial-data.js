b972829b1abdb6490084bf1946153f63376fdbae
Melvin Mallari <mtmallari@ucdavis.edu>
2019-05-14 14:59:44 -0700

    Finalize Styling

--- src/initial-data.js
+++ src/initial-data.js
@@ -1,7 +1,7 @@
 const initialData = {
   tasks: {
     'task-1': { id: 'task-1', content: 'Create Columns' },
-    'task-2': { id: 'task-2', content: 'Create add task form' },
+    'task-2': { id: 'task-2', content: 'Create Column' },
     'task-3': { id: 'task-3', content: 'Create add task form' },
     'task-4': { id: 'task-4', content: 'Create task' },
     'task-5': { id: 'task-5', content: 'Style' }
